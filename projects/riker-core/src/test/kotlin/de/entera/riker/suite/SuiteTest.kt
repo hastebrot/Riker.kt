5bbfff0e615de14b744996932c354d3dd36bdce1
hastebrot <hastebrot@gmail.com>
2015-11-11 15:35:37 +0100

    (test) Add test case for tests with setup and cleanup.

--- projects/riker-core/src/test/kotlin/de/entera/riker/suite/SuiteTest.kt
+++ projects/riker-core/src/test/kotlin/de/entera/riker/suite/SuiteTest.kt
@@ -5,13 +5,40 @@ import kotlin.test.expect
 
 class SuiteTest {
     @Test
-    fun `transform() test`() {
+    fun `transform() test test`() {
         // given:
         val source = Node("suite",
+            Node("test"),
             Node("test")
         )
         val expected = Node("suite",
             Node("testCase",
+                Node("test")
+            ),
+            Node("testCase",
+                Node("test")
+            )
+        )
+
+        // expect:
+        expect(expected) { transform(source) }
+    }
+
+    @Test
+    fun `transform() testSetup test test`() {
+        // given:
+        val source = Node("suite",
+            Node("testSetup"),
+            Node("test"),
+            Node("test")
+        )
+        val expected = Node("suite",
+            Node("testCase",
+                Node("testSetup"),
+                Node("test")
+            ),
+            Node("testCase",
+                Node("testSetup"),
                 Node("test")
             )
         )
@@ -32,8 +59,13 @@ data class Node<T>(val data: T,
 fun transform(node: Node<String>): Node<String> {
     val target = Node(node.data)
 
+    // collect setups and cleanups.
+    val testSetups = node.children.filter { it.data == "testSetup" }
+    val testCleanups = node.children.filter { it.data == "testCleanup" }
+
+    // populate test cases.
     val testCases = node.children.filter { it.data == "test" }
-                                 .map { Node("testCase", it ) }
+        .map { Node("testCase", (testSetups + it + testCleanups).toArrayList() ) }
     target.children.addAll(testCases)
 
     return target
